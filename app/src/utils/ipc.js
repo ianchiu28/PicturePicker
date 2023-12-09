7b02a0afb2fb6df5b52ccf038fc331777516b487
Ian Chiu <nk9710632@gmail.com>
2023-12-09 21:32:56 +0800

    refactor: ipc

--- app/src/utils/ipc.js
+++ app/src/utils/ipc.js
@@ -3,26 +3,31 @@ const { ipcMain } = require("electron");
 const pictureUtils = require("./picture");
 const WindowSingleton = require("./window");
 
+const savePictures = async () => {
+	const folderPath = await WindowSingleton.getInstance().openDirectory();
+	if (!folderPath) return;
+	
+	const type = await pictureUtils.savePicturesToDB(folderPath);
+	await pictureUtils.loadPicturesFromDB(type);
+};
+
+const reloadPictures = (_event, index, rank) => {
+	const picturesInfo = pictureUtils.getPicturesByIndex(index, rank);
+	const picturesRankMap = pictureUtils.getPicturesRankMap();
+	return { ...picturesInfo, picturesRankMap };
+};
+
+const updateRank = async (_event, index, rank, score) => {
+	await pictureUtils.updatePictureRank(index, rank, score);
+};
+
+const exportHighestRankPictures = pictureUtils.exportHighestRankPictures;
+
 function initializeIpc () {
-	ipcMain.handle("savePictures", async () => {
-		const folderPath = await WindowSingleton.getInstance().openDirectory();
-		if (!folderPath) return;
-		
-		const type = await pictureUtils.savePicturesToDB(folderPath);
-		await pictureUtils.loadPicturesFromDB(type);
-	});
-
-	ipcMain.handle("reloadPictures", (_event, index, rank) => {
-		const picturesInfo = pictureUtils.getPicturesByIndex(index, rank);
-		const picturesRankMap = pictureUtils.getPicturesRankMap();
-		return { ...picturesInfo, picturesRankMap };
-	});
-
-	ipcMain.handle("updateRank", async (_event, index, rank, score) => {
-		await pictureUtils.updatePictureRank(index, rank, score);
-	});
-
-	ipcMain.handle("exportHighestRankPictures", pictureUtils.exportHighestRankPictures);
+	ipcMain.handle("savePictures", savePictures);
+	ipcMain.handle("reloadPictures", reloadPictures);
+	ipcMain.handle("updateRank", updateRank);
+	ipcMain.handle("exportHighestRankPictures", exportHighestRankPictures);
 }
 
 module.exports = { initializeIpc };
