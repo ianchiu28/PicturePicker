b7f0fe425ce0e0702f66e581c9b20676fa1203bb
Ian Chiu <nk9710632@gmail.com>
2023-12-09 23:25:44 +0800

    refactor: ipc handler

--- app/electron/ipc.js
+++ app/electron/ipc.js
@@ -2,22 +2,17 @@ const { ipcMain } = require("electron");
 
 const pictureService = require("../src/services/picture.service");
 
-const savePictures = async () => 
-	pictureService.savePicturesToDB();
-
-const reloadPictures = (_event, index, rank) =>
-	pictureService.reloadPictures(index, rank);
-
-const updateRank = async (_event, index, rank, score) =>
-	pictureService.updatePictureRank(index, rank, score);
-
-const exportHighestRankPictures = pictureService.exportHighestRankPictures;
-
-const initializeIpc = () => {
-	ipcMain.handle("savePictures", savePictures);
-	ipcMain.handle("reloadPictures", reloadPictures);
-	ipcMain.handle("updateRank", updateRank);
-	ipcMain.handle("exportHighestRankPictures", exportHighestRankPictures);
+const IPC_FUNCTIONS_MAP = {
+	"savePictures": pictureService.savePicturesToDB,
+	"reloadPictures": pictureService.reloadPictures,
+	"updateRank": pictureService.updatePictureRank,
+	"exportHighestRankPictures": pictureService.exportHighestRankPictures
 };
+
+const setIpcHandle = ([key, func]) =>
+	ipcMain.handle(key, (_event, ...args) => func(...args));
+
+const initializeIpc = () =>
+	Object.entries(IPC_FUNCTIONS_MAP).forEach(setIpcHandle);
 
 module.exports = { initializeIpc };
